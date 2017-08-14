d74d466d6ff84c1d5188efb8b874eeae8ae397fe
Michael Klein <miketheklein@gmail.com>
2017-08-14 15:29:01 -0700

    wrote out framework for test

---
+++ part-2/database_test.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const expect = chai.expect
+const queries = require('./hotel.js')
+
+describe( 'Guests', function(){
+  it('list all guests.', function(){
+    
+  })
+})
+describe( 'Rooms', function(){
+  it('list all rooms.', function(){
+    
+  })
+  it('list only rooms with availability when --available option is provided.', function(){
+    
+  })
+})
+describe( 'Bookings', function(){
+  it('list all upcoming room bookings (soonest check-in date first)).', function(){
+    
+  })
+  it('list bookings for one room with the soonest check-in date first when room argument is provided.', function(){
+    
+  })
+  it('list all upcoming room bookings (soonest check-in date first).', function(){
+    
+  })
+})
