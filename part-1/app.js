e49a4874c98ee988bc0da9ceb574de5bb9028acd
Michael Klein <miketheklein@gmail.com>
2017-08-14 13:01:41 -0700

    Merge functionality working for good cases

--- part-1/app.js
+++ part-1/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const api = require('./routes.js')
 
--- part-1/routes.js
+++ part-1/routes.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/shout/:word', (request, response, next) => {
   const { word } = request.params;
-  console.log( '---===word===---', word );
-  response.send( word.toUpperCase() + '!!!')
-  response.status = 200
+  response.status(200).send( word.toUpperCase() + '!!!')
   next()
 })
 
-router.post('/array/merge', (request, response) => {
+router.post('/array/merge', (request, response, next) => {
   
+  const a = request.body.a
+  const b = request.body.b
+  if (a !typeof Array || b !typeof Array) {
+    const error = new Error()
+    response.send({error})
+  } else{
+    var result = a.reduce( (arr, v, i) => {
+      return arr.concat(v, b[i]); 
+    }, []);
+    response.send( {result} )
+    next()
+  }
 })
 
 
